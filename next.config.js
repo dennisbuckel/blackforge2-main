0e06a4845ca1244b83e7e5e5946c7d0688c9d229
dennisbuckel <dennis.buck@web.de>
2025-04-08 15:38:10 +0200

    Initial commit without node_modules

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 // next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Aktiviert statischen Export
-  images: {
-    unoptimized: true, // Für statischen Export notwendig
-  },
-}
+  // Aktiviert den React Strict Mode (empfohlen für das Aufspüren von Fehlern)
+  reactStrictMode: true,
 
-module.exports = nextConfig
+  // Nutzt den SWC-Minifier für kleinere Builds
+  swcMinify: true,
+
+  // Für statischen Export notwendig, wenn du Next.js-Image-Komponenten verwendest
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
