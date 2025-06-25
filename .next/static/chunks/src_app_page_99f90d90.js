ee255a8d888f25230caaaaaa48ac22247d0e7fb7
dennisbuckel <dennis.buck@web.de>
2025-06-25 21:59:47 +0200

    Remove .next build files from Git tracking

--- .next/static/chunks/src_app_page_99f90d90.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_components_VideoHeroWrapper_c41e9a0e.js",
-  "static/chunks/_60d90352._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_components_VideoHeroWrapper_5f747851.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_components_videoHero_module_c22f1cd7.css",
-  "static/chunks/src_components_e027d81a._.js"
-],
-    source: "dynamic"
-});
